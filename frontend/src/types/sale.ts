cf6ff8f83d4ca002fbf4b51a2a45890a643b411b
Leonardo Klinger <gamefivebr@gmail.com>
2021-09-10 22:49:39 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,11 +1,32 @@
-import { type } from "os"
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean,
+    totalElements: number;
+    totalPage: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
 
 export type SaleSum = {
     sellerName: string;
     sum: number;
 }
 
-export type SaleSucess =    {
+export type SaleSucess = {
     sellerName: string;
     visited: number;
     deals: number;
